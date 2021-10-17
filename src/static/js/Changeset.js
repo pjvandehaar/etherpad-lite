de86bd358a23e3229f2fd0a195797306ad582f0b
Richard Hansen <rhansen@rhansen.org>
2021-10-16 22:06:06 -0400

    Changeset: Avoid unnecessary `StringAssembler` class

--- src/static/js/Changeset.js
+++ src/static/js/Changeset.js
@@ -730,7 +730,10 @@ class StringAssembler {
 /**
  * @returns {StringAssembler}
  */
-exports.stringAssembler = () => new StringAssembler();
+exports.stringAssembler = () => {
+  warnDeprecated('Changeset.stringAssembler() is deprecated; build a string manually instead');
+  return new StringAssembler();
+};
 
 /**
  * Class to iterate and modify texts which have several lines. It is used for applying Changesets on
@@ -1142,7 +1145,7 @@ exports.applyToText = (cs, str) => {
   assert(str.length === unpacked.oldLen, 'mismatched apply: ', str.length, ' / ', unpacked.oldLen);
   const bankIter = exports.stringIterator(unpacked.charBank);
   const strIter = exports.stringIterator(str);
-  const assem = new StringAssembler();
+  let assem = '';
   for (const op of new exports.OpIter(unpacked.ops)) {
     switch (op.opcode) {
       case '+':
@@ -1151,7 +1154,7 @@ exports.applyToText = (cs, str) => {
         if (op.lines !== bankIter.peek(op.chars).split('\n').length - 1) {
           throw new Error(`newline count is wrong in op +; cs:${cs} and text:${str}`);
         }
-        assem.append(bankIter.take(op.chars));
+        assem += bankIter.take(op.chars);
         break;
       case '-':
       // op is - and op.lines 0: no newlines must be in the deleted string
@@ -1167,12 +1170,12 @@ exports.applyToText = (cs, str) => {
         if (op.lines !== strIter.peek(op.chars).split('\n').length - 1) {
           throw new Error(`newline count is wrong in op =; cs:${cs} and text:${str}`);
         }
-        assem.append(strIter.take(op.chars));
+        assem += strIter.take(op.chars);
         break;
     }
   }
-  assem.append(strIter.take(strIter.remaining()));
-  return assem.toString();
+  assem += strIter.take(strIter.remaining());
+  return assem;
 };
 
 /**
@@ -1247,12 +1250,11 @@ exports.composeAttributes = (att1, att2, resultIsMutation, pool) => {
     }
     return '';
   });
-  const buf = new StringAssembler();
+  let buf = '';
   for (const att of [...atts].sort((a, b) => (a[0] > b[0]) - (a[0] < b[0]))) {
-    buf.append('*');
-    buf.append(exports.numToString(pool.putAttrib(att)));
+    buf += `*${exports.numToString(pool.putAttrib(att))}`;
   }
-  return buf.toString();
+  return buf;
 };
 
 /**
@@ -1468,7 +1470,7 @@ exports.compose = (cs1, cs2, pool) => {
   const len3 = unpacked2.newLen;
   const bankIter1 = exports.stringIterator(unpacked1.charBank);
   const bankIter2 = exports.stringIterator(unpacked2.charBank);
-  const bankAssem = new StringAssembler();
+  let bankAssem = '';
 
   const newOps = applyZip(unpacked1.ops, unpacked2.ops, (op1, op2) => {
     const op1code = op1.opcode;
@@ -1478,16 +1480,12 @@ exports.compose = (cs1, cs2, pool) => {
     }
     const opOut = slicerZipperFunc(op1, op2, pool);
     if (opOut.opcode === '+') {
-      if (op2code === '+') {
-        bankAssem.append(bankIter2.take(opOut.chars));
-      } else {
-        bankAssem.append(bankIter1.take(opOut.chars));
-      }
+      bankAssem += (op2code === '+' ? bankIter2 : bankIter1).take(opOut.chars);
     }
     return opOut;
   });
 
-  return exports.pack(len1, len3, newOps, bankAssem.toString());
+  return exports.pack(len1, len3, newOps, bankAssem);
 };
 
 /**
@@ -1902,7 +1900,7 @@ exports.Builder = class {
   constructor(oldLen) {
     this._oldLen = oldLen;
     this._ops = [];
-    this._charBank = new StringAssembler();
+    this._charBank = '';
   }
 
   /**
@@ -1925,7 +1923,7 @@ exports.Builder = class {
 
   insert(text, attribs, pool) {
     this._ops.push(...opsFromText('+', text, attribs, pool));
-    this._charBank.append(text);
+    this._charBank += text;
     return this;
   }
 
@@ -1944,7 +1942,7 @@ exports.Builder = class {
       lengthChange = yield* exports.canonicalizeOps(this._ops, true);
     }).call(this));
     const newLen = this._oldLen + lengthChange;
-    return exports.pack(this._oldLen, newLen, serializedOps, this._charBank.toString());
+    return exports.pack(this._oldLen, newLen, serializedOps, this._charBank);
   }
 };
 
@@ -2103,20 +2101,20 @@ exports.inverse = (cs, lines, alines, pool) => {
 
   const nextText = (numChars) => {
     let len = 0;
-    const assem = new StringAssembler();
+    let assem = '';
     const firstString = linesGet(curLine).substring(curChar);
     len += firstString.length;
-    assem.append(firstString);
+    assem += firstString;
 
     let lineNum = curLine + 1;
     while (len < numChars) {
       const nextString = linesGet(lineNum);
       len += nextString.length;
-      assem.append(nextString);
+      assem += nextString;
       lineNum++;
     }
 
-    return assem.toString().substring(0, numChars);
+    return assem.substring(0, numChars);
   };
 
   const cachedStrFunc = (func) => {
@@ -2329,12 +2327,9 @@ const followAttributes = (att1, att2, pool) => {
     return '';
   });
   // we've only removed attributes, so they're already sorted
-  const buf = new StringAssembler();
-  for (const att of atts) {
-    buf.append('*');
-    buf.append(exports.numToString(pool.putAttrib(att)));
-  }
-  return buf.toString();
+  let buf = '';
+  for (const att of atts) buf += `*${exports.numToString(pool.putAttrib(att))}`;
+  return buf;
 };
 
 exports.exportedForTestingOnly = {
